d100c3701c36951e492a53e0df9a030ac0acb18a
XYXShawn <58592035+XYXShawn@users.noreply.github.com>
2020-01-16 13:03:36 +0800

    Update qyxj.js

--- qyxj.js
+++ qyxj.js
@@ -1,3 +1,16 @@
+/*
+qingyanxiangji
+
+Surge 4.0 :
+[Script]
+http-response ^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/ requires-body=1,max-size=0,script-path=script/qyxj.js
+~~~~~~~~~~~~~~~~
+Surge MITM = commerce-api.faceu.mobi
+
+
+*/
+
+
 let obj = JSON.parse($response.body);
 obj ={
 "ret":"0",
